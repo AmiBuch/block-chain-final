b52768bba6bf9a45829f57d05da07f284e7d238a
Ami Buch <64002199+AmiBuch@users.noreply.github.com>
2020-08-04 13:57:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{MedBlockChain}}'));
+app.use(express.static('./dist/PatientBC'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/{{MedBlockChain}}/index.html'));
+res.sendFile(path.join(__dirname,'/dist/PatientBC/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
